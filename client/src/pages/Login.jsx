a0e607e17dc435bc9568460b592b5c776cada144
Nerzzz <guilhermeschiavolnsantos@gmail.com>
2026-04-01 15:42:15 -0300

    Inicio da interface e mudanças no server

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,19 @@
+import "../styles/login.css"
+
+function Login() {
+  return (
+    <main>
+        <div className="login_container">
+            <h1>Login</h1>
+            <p>Preencha seus dados e acesse o sistema!</p>
+            <form action="" name="formLogin">
+                <input type="text" name="login" placeholder="johndoe@example.com" />
+                <input type="password" name="password" placeholder="Senha" />
+                <input type="submit" value="Entrar" />
+            </form>
+        </div>
+    </main>
+  )
+}
+
+export default Login
